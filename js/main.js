3f27417c1d864a6daedd0a2760c5cd5c542345cd
Alejandro Landaverde <adlandav@ucsc.edu>
2019-05-12 19:09:57 -0700

    centerd the game, and also changed the screen to 1280x720

--- js/main.js
+++ js/main.js
@@ -7,10 +7,11 @@ var blueJewel = false;
 var yellowJewel = false;
 var keys1 = false;
 var keys2 = false;
+game = new Phaser.Game(1280, 720, Phaser.AUTO, "finalProto", { preload: preload });
 
-window.onload = function() {
-	game = new Phaser.Game(640, 640, Phaser.AUTO, "finalProto");
-
+window.onload = function () {
+	game.scale.pageAlignHorizontally = true;
+	game.scale.pageAlignVertically = true;
 	// define states
 	game.state.add("Boot", Boot);
 	game.state.add("Load", Load);
@@ -19,13 +20,17 @@ window.onload = function() {
 	game.state.add("GameOver", GameOver);
 	game.state.start("Boot");
 }
-function getDistanceBetween2Points(a, b) {//last 2 values are a number and a boolean and are optional
+function getDistanceBetween2Points(a, b) {
 	var x = a.x - b.x;
 	var y = a.y - b.y;
 	return Math.sqrt(x * x + y * y);
 }
 function isInRange(a, b, range) {//takes 2 points and the value of the range.
-	var x = (a.x - b.x < 0)? -(a.x - b.x) : (a.x - b.x);
-	var y = (a.y - b.y < 0)? -(a.y - b.y) : (a.y - b.y);
-	return (x < range && y < range)? true: false;
+	var x = (a.x - b.x < 0) ? -(a.x - b.x) : (a.x - b.x);
+	var y = (a.y - b.y < 0) ? -(a.y - b.y) : (a.y - b.y);
+	return (x < range && y < range) ? true : false;
+}
+function preload() {
+	//to center game
+	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 }
