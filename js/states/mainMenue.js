d002c9850ad5bbad6e8af13b641207d4f7cd345b
Andrea Christine Zarevich <azarevic@ucsc.edu>
2019-05-30 09:13:29 -0700

    changed text

--- js/states/mainMenue.js
+++ js/states/mainMenue.js
@@ -8,7 +8,7 @@ MainMenue.prototype = {
 		// add title screen text
 		var titleText1 = game.add.text(game.world.width/2, 200, "[Title]\n", {font: 'Bookman', fontSize: '64px', fill: '#fff'});
 		var titleText2 = game.add.text(game.world.width/2, game.world.height/2, "Press [SPACE] to start\n", {font: 'Bookman', fontSize: '48px', fill: '#fff'});
-		var titleText3 = game.add.text(game.world.width/2, (game.world.height/2)+75, "Press [T] for tutorial\n", {font: 'Bookman', fontSize: '48px', fill: '#fff'});
+		var titleText3 = game.add.text(game.world.width/2, (game.world.height/2)+75, "Press [T] for Controls\n", {font: 'Bookman', fontSize: '48px', fill: '#fff'});
 		var titleText4 = game.add.text(game.world.width/2, (game.world.height/2)+150, "Press [C] for credits\n", {font: 'Bookman', fontSize: '48px', fill: '#fff'});
 		titleText1.anchor.set(0.5);
 		titleText2.anchor.set(0.5);
@@ -38,7 +38,7 @@ MainMenue.prototype = {
 		if(game.input.keyboard.justPressed(Phaser.Keyboard.C)) {
 			game.state.start("Credits");
 		}
-		if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBA)) {
+		if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)) {
 			game.state.start("Play");
 		}
 	}
