979775685398c8197485b9216af37a1f120e3093
Alejandro Landaverde <adlandav@ucsc.edu>
2019-06-06 16:37:03 -0700

    Created note objects, and a function that tells the player to press E once the player is close. pressing E rn doesnt do anything tho

--- js/prefabs/note.js
+++ js/prefabs/note.js
@@ -1,15 +1,24 @@
-var note = function (game, key, xpos, ypos, text) {
+var note = function (game, key, xpos, ypos, text, player) {
     myObj.call(this, game, key, xpos, ypos);
-    this.text = text;
-    // this.scale.x = 0.025;
-    // this.scale.y = 0.025;
-    // this.name = key;
-    // this.id = id;
-    this.body.onCollide = new Phaser.Signal();
-    this.body.onCollide.add(hitNote, this);
+    this.mytext = text;
+    this.scale.x = 0.025;
+    this.scale.y = 0.025;
+    style = { font: '40px Almendra', fill: '#fff', align: 'center' };
+    this.InstructionText = game.add.text(0, 0, "Press 'E'", style);
+    this.InstructionText.kill();
+    this.range = 32;
+    this.player = player;
 }
 note.prototype = Object.create(myObj.prototype);
 note.prototype.constructor = note;
-function hitNote (note, player) {
-    console.log("note hit");
+note.prototype.update = function() {
+    if (isInRange(this.body.position, this.player.body.position, this.range)) {
+        this.InstructionText.x = this.body.position.x - 32; 
+        this.InstructionText.y = this.body.position.y + 32; 
+
+        this.InstructionText.revive();
+    }
+    else {
+        this.InstructionText.kill();
+    }
 }
